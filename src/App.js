d5d9a61d10cfff6aac3539bd6101d590be803b9f
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-06-28 23:11:52 +0300

    react-router-dom was added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
 function App() {
-  return <>Hello from App</>;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
+  );
 }
 
 export default (process.env.NODE_ENV === 'development' ? hot(App) : App);
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,7 @@
+import React, { PureComponent } from 'react';
+
+export default class SingleRoom extends PureComponent {
+  render() {
+    return <div>SingleRoom page</div>;
+  }
+}
