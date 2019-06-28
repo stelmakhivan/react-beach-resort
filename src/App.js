903c93612c91fbe1363e4656b0a07bec0a4fd6c3
Ivan Stelmakh <stelmahivan1994@gmail.com>
2019-06-28 22:41:46 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import { hot } from 'react-hot-loader/root';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>Hello from App</>;
 }
 
-export default App;
+export default (process.env.NODE_ENV === 'development' ? hot(App) : App);
